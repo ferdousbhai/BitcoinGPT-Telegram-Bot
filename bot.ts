6841bf33f503707cb1972aade129eacdb6633945
Ferdous Bhai <ferdousbd@gmail.com>
2023-03-07 11:31:54 +0800

    Implement ChatGPT with a simple system instruction to give it Bitcoin focused and funny.

--- bot.ts
+++ bot.ts
@@ -34,7 +34,7 @@ async function fetchChatGPT(
 const chatMessages = [{
   "role": "system",
   "content":
-    "You are a fan of Bitcoin and Ethereum. You respond with dark humor whenever possible.",
+    "You are a fan of Bitcoin. You always respond with dark humor.",
 }];
 
 // Listen for messages
@@ -45,15 +45,18 @@ bot.on("message", async (ctx) => {
   if (messageText && messageText.trim()) {
     chatMessages.push({ "role": "user", "content": messageText });
   }
-  // Only keep the system message and the last 5 user messages:
-  if (chatMessages.length > 6) {
-    chatMessages.splice(1, 6 - chatMessages.length);
+  // Only keep the system message but set it up so that this can be easily changed to keep more messages later.
+  if (chatMessages.length > 2) {
+    chatMessages.splice(1, chatMessages.length - 2);
   }
+  console.log(`Chat messages: (${chatMessages.length})`)
+  console.log(chatMessages)
   // Send the chat messages as context to the OpenAI API
   const generatedText = await fetchChatGPT(chatMessages);
   // Reply with generated text
   try {
     await ctx.reply(generatedText);
+    chatMessages.push({ "role": "assistant", "content": generatedText });
   } catch (error) {
     console.log(error);
   }
