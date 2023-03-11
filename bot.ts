55393b0749be053eb66f8aa455e17945a50cfdd3
Ferdous Bhai <ferdousbd@gmail.com>
2023-03-11 09:57:45 +0800

    Log chat buffer prior to making API call

--- bot.ts
+++ bot.ts
@@ -17,6 +17,7 @@ bot.on("message", async (ctx) => {
     await ctx.api.sendChatAction(ctx.chat.id, "typing");
     // Update the chat buffer with the user's message
     chatBuffer.push({ role: "user", content: messageText! });
+    console.log(chatBuffer); // Log the chat buffer
     // Call the ChatGPT API to generate a response
     const completionText = await fetchChatGPT([
       {
@@ -33,7 +34,6 @@ bot.on("message", async (ctx) => {
     if (chatBuffer.length > CHAT_TURN_BUFFER_SIZE * 2) {
       chatBuffer.splice(0, 2);
     }
-    console.log(chatBuffer);
   }
 });
 
